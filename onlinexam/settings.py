442573faad0cbaccc55830788ea7fb18972fa5fa
Anishraut37 <rautanish1901@@gmail.com>
2025-06-08 12:54:50 +0530

    Add homepage view and allowed hosts fix

--- onlinexam/settings.py
+++ onlinexam/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = '@k0#p3kidu)yaaa3u1hplxz)f@^6xiy384*(+n@@s5x#1bx@m5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['examinationsystem.onrender.com']
+ALLOWED_HOSTS = ['examinationsystem-1.onrender.com']
+
+
 
 
 
